c0cd425bca558ffc7c9545371ef2d36d8aabc437
EdierChi <77143200+EdierChi@users.noreply.github.com>
2025-08-24 02:10:05 -0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import polloImg from "./Pollo.jpg";
 import resImg from "./Res.jpg";
 import cerdoImg from "./Cerdo.jpg";
 import pescadoImg from "./Pescado.jpg";
-import logo from "./logo.png";
+import logo from "./logo.png.png";
 
 const CheckIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
